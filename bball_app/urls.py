f2bca9c496984c19113dde7552ee1cbcad4515b0
Michael.Erickson <merickson51293@gmail.com>
2021-02-09 08:41:09 -0500

    added homepage for going to mens team and womens team

--- bball_app/urls.py
+++ bball_app/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns=[
     path('', views.index),
+    path('homepage', views.homepage),
+    path('whomepage', views.whomepage),
 ]
--- bball_app/views.py
+++ bball_app/views.py
@@ -4,3 +4,9 @@ from django.http import HttpResponse, HttpResponseRedirect
 
 def index(request):
     return render(request, "index.html")
+
+def homepage(request):
+    return render(request, "home.html")
+
+def whomepage(request):
+    return render(request, "whome.html")
